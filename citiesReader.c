15c7b66b16d8d546cbc1ef8ca55f4963b04c38b3
Z-mh666 <78141281+Z-mh666@users.noreply.github.com>
2021-05-13 21:57:51 +0800

    citiesReader de plus grand ville de dpt ajouté

--- citiesReader.c
+++ citiesReader.c
@@ -96,6 +96,118 @@ ListOfCities* citiesReader(int popMin, int dpt){
   return cities;
 }
 
+
+ListOfCities* bigcitiesReader(){
+
+  ListOfCities* cities = malloc(sizeof(ListOfCities));
+
+//--------------------------------------------------------------------
+//--- READING the largest city of every provinces
+//--------------------------------------------------------------------
+
+  printf("== Reading the biggest city of every province ==\n");
+  FILE* inputFile = NULL;
+  inputFile = fopen("citiesList.csv", "r");
+  if(inputFile != NULL){
+
+    char line[512];
+    const char s[2] = ",";
+    char *token;
+    
+    cities->number = 94; //There are 94 provinces in the interior of France
+
+    // Allocate arrays
+    cities->dpt  = malloc(cities->number*sizeof(int));
+    cities->name = malloc(cities->number*sizeof(char*));
+    cities->pop  = malloc(cities->number*sizeof(int));
+    cities->lon  = malloc(cities->number*sizeof(float));
+    cities->lat  = malloc(cities->number*sizeof(float));
+    
+    // Reading/Saving data
+    int index=0;
+    int Dpt = 1;
+    int Pop = 100;
+    float Lon = 0.0;
+    float Lat = 0.0;
+    char Name[32];
+    char lastcity[10];
+    strcpy(lastcity,"FOSSES"); //the last city of the list
+    char *myName = malloc(32*sizeof(char));
+    while(fgets(line, 512, inputFile) != NULL){
+      token = strtok(line, s);
+      token = strtok(NULL, s);
+      int myDpt = atoi(token);
+      for(int i=0; i<2;  i++) token = strtok(NULL, s);
+      strncpy(myName, token, 32);
+      for(int i=0; i<11; i++) token = strtok(NULL, s);
+      int myPop = atoi(token);
+      for(int i=0; i<5;  i++) token = strtok(NULL, s);
+      float myLon = atof(token);
+      for(int i=0; i<1;  i++) token = strtok(NULL, s);
+      float myLat = atof(token);
+      
+      //the largest city of the province
+      if (myDpt == Dpt){
+        if (myPop >= Pop){
+          Pop = myPop;
+          Lon = myLon;
+          Lat = myLat;
+          strcpy(Name, myName);
+        }
+      }
+
+      //put the detail of the largest city of the province in the list
+      if (myDpt != Dpt){
+        cities->dpt[index] = Dpt;
+        cities->name[index] = (char*) malloc(32*sizeof(char));
+        strncpy(cities->name[index], Name, 32);
+        cities->pop[index] = Pop;
+        cities->lon[index] = Lon;
+        cities->lat[index] = Lat;
+        index++;
+        Pop = 100;
+        Lon = 0.0;
+        Lat = 0.0;
+        Dpt += 1;
+        if (Dpt == 20){
+          Dpt += 1;
+        }
+      }
+
+      //if all the cities has been read
+      if (strcmp(lastcity,myName)==0){
+        cities->dpt[index] = Dpt;
+        cities->name[index] = (char*) malloc(32*sizeof(char));
+        strncpy(cities->name[index], Name, 32);
+        cities->pop[index] = Pop;
+        cities->lon[index] = Lon;
+        cities->lat[index] = Lat;
+      }
+    }
+    free(myName);
+    fclose(inputFile);
+  }
+
+//--------------------------------------------------------------------
+//--- WRITING largest cities of every provinces
+//--------------------------------------------------------------------
+
+  printf("== Writing largest cities of every provinces in 'resuCities.dat' ==\n");
+
+  FILE* outputFile = NULL;
+  outputFile = fopen("resuCities.dat", "w");
+  if(outputFile != NULL){
+    for(int i=0; i<cities->number; i++){
+      fprintf(outputFile, "%i %f %f\n", cities->pop[i], cities->lon[i], cities->lat[i]);
+    }
+    fclose(outputFile);
+  }
+
+  return cities;
+
+}
+
+
 float poids(ListOfCities* cities, int a, int b){
   int R = 6378;
   float pi = 3.14;
