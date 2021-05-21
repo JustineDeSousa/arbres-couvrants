9f6bf57a07071650cfa681044ab33273696baf34
JustineDeSousa <justine.desousa6@gmail.com>
2021-05-21 20:45:13 +0200

    .

--- visualisation_variante.py
+++ visualisation_variante.py
@@ -4,16 +4,27 @@
 from matplotlib import pyplot
 from numpy import loadtxt
 
-# draw the cities
-cities = loadtxt("resuCities.dat", dtype=float, delimiter=" ")
-pyplot.scatter(cities[:,1], cities[:,2], s=cities[:,0]/1000, c=cities[:,0], alpha=0.5);
+cities = loadtxt("resuBigCities.dat", dtype=float, delimiter=" ")
 
-# draw the graph
-graph = loadtxt("resuGraph.dat", dtype=int)
+# draw the graph big cities
+graph = loadtxt("resuGraph_bigcities.dat", dtype=int)
 print(graph.shape[0])
 for x in range(graph.shape[0]):
   edge = [graph[x,0], graph[x,1]]
   pyplot.plot(cities[edge,1], cities[edge,2], 'b')
+
+
+# draw the cities
+cities = loadtxt("resuCities.dat", dtype=float, delimiter=" ")
+pyplot.scatter(cities[:,1], cities[:,2], s=cities[:,0]/1000, c=cities[:,0], alpha=0.5)
+
+
+# draw the graph
+graph = loadtxt("resuGraph.dat", dtype=int)
+for x in range(graph.shape[0]):
+  edge = [graph[x,0], graph[x,1]]
+  pyplot.plot(cities[edge,1], cities[edge,2], 'b')
+
 
 pyplot.xlabel('Longitude', size=16)
 pyplot.ylabel('Latitude', size=16)
